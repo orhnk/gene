8612a80a5044a96dfa81b52aaeb8c4ef5af76f8c
utfeight <utfeightt@gmail.com>
2023-11-28 05:27:13 +0300

    feat: base file-tree

---
+++ core/src/lib.rs
@@ -0,0 +1,7 @@
+//! # GENE - Generic Package Manager
+//!
+//! ## Programming Specific Features
+//!
+//! With GENE, you have the ability to use any dependency with any programming language.
+//! For example,
+//! You can use bare gtk with rust. Thanks to the auto-binding generators that GENE uses.
