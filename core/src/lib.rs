bfe4a9cf126dfe1dc9824adb9dc93bc6ecf7d2db
utfeight <utfeightt@gmail.com>
2023-12-02 12:19:41 +0300

    chore: initialize file-tree

--- core/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! # GENE - Generic Package Manager
-//!
-//! ## Programming Specific Features
-//!
-//! With GENE, you have the ability to use any dependency with any programming language.
-//! For example,
-//! You can use bare gtk with rust. Thanks to the auto-binding generators that GENE uses.
