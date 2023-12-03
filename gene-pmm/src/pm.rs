b9cba0a58819f7695d7a9826d583326e615d6d98
utfeight <utfeightt@gmail.com>
2023-12-03 11:59:25 +0300

    refactor: add compile function to PackageManagers

--- gene-pmm/src/pm.rs
+++ gene-pmm/src/pm.rs
@@ -1,6 +1,9 @@
 use crate::gene::GeneArgs;
 
 pub trait PackageManager {
-	/// Translate gene representation to Self
-	fn from_gene(args: GeneArgs) -> Self;
+    /// Translate gene representation to Self
+    fn from_gene(args: &GeneArgs) -> Self;
+
+    /// Convert Middle Representation (Self) to String
+    fn compile(&self) -> String;
 }
