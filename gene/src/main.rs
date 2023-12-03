ca8928790184f765c3ee0a3da8e821da53a970b6
utfeight <utfeightt@gmail.com>
2023-12-03 12:55:22 +0300

    refactor: main function for idiomatic rust
    Removed pm::PackageManager import from gene_pmm and refactored main function in gene/src/main.rs. Improved the function by replacing 'from_gene' method with 'From<&'a GeneArgs>'. This update aligns better with Rust idiomatic practices and enhances PackageManager trait usability.

--- gene/src/main.rs
+++ gene/src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
-use gene_pmm::{gene::GeneArgs, pm::PackageManager, spm::pacman::*};
+use gene_pmm::{gene::GeneArgs, spm::pacman::*};
 
 fn main() {
-    let args = GeneArgs::parse();
-    let pacman_tranlation = Pacman::from_gene(&args);
-    println!("{:#?}", pacman_tranlation);
+	let args = GeneArgs::parse();
+	let pacmane_repr = Pacman::from(&args);
+	println!("{:#?}", pacmane_repr);
 }
