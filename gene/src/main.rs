47f9d1c413f36a9a4255196e351f3db2417c6836
utfeight <utfeightt@gmail.com>
2023-12-03 19:07:17 +0300

    fix: remove unnecessary 'lib' module and improve indentation
    Removed the 'lib' module from the main.rs file as it was no longer needed. Also improved the code indentation for better readability and consistency. This commit is focused on improving the codebase maintainability and readability.

--- gene/src/main.rs
+++ gene/src/main.rs
@@ -1,9 +1,7 @@
-use anyhow::Context;
 use clap::Parser;
 use gene_utils::args::GeneArgs;
 use gene_pmm::spm::pacman::*;
 
-mod lib;
 
 // NOTE: I wanted to use `anyhow` for error handling, but it doesn't work with limited lifetimes
 // (dynamic dispatch makes it impossible to know what the lifetime of the variable is)
@@ -11,7 +9,7 @@ mod lib;
 // TODO: improve overall error handling
 
 fn main() {
-	let args: GeneArgs = GeneArgs::parse();
-	let pacman_repr = Pacman::try_from(&args).expect("failed to create pacman");
-	println!("{:#?}", pacman_repr);
+    let args: GeneArgs = GeneArgs::parse();
+    let pacman_repr = Pacman::try_from(&args).expect("failed to create pacman");
+    println!("{:#?}", pacman_repr);
 }
