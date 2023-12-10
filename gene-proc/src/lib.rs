c35d34c148905a8137d793edd01559d64c1465d6
utfeight <utfeightt@gmail.com>
2023-12-10 12:10:59 +0300

    refactor: revise gene-proc derive macro and enhance functionality
    This commit refactors the derive macro in the gene-proc library. Renamed the function to 'derive_name' and made it more versatile to handle not only lifetimes but also non-lifetime variables. Made changes to return lower cased name of the struct as a Static Str. Now this macro will allow users to derive a 'name' function that returns the name of the struct in lower case.

--- gene-proc/src/lib.rs
+++ gene-proc/src/lib.rs
@@ -1,19 +1,49 @@
-extern crate proc_macro;
-
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Name)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive_name(input: TokenStream) -> TokenStream {
 	let ast = parse_macro_input!(input as DeriveInput);
-	let name = &ast.ident;
+	let name = ast.ident;
+	let lovercase_name = name.to_string().to_lowercase();
+
+	let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
 	let gen = quote! {
-        impl #name {
-            pub fn name() -> String {
-                stringify!(#name).trim().to_lowercase()
+		impl #impl_generics #name #ty_generics #where_clause {
+			#[inline(always)]
+            pub fn name() -> &'static str {
+				#lovercase_name
             }
         }
     };
+
 	gen.into()
 }
+/*
+
+#[derive(Name)] // This derive macro
+struct Foo<'a> {
+	foo: &'a str,
+}
+
+// Expands to this code:
+
+impl Foo<'_> {
+	pub fn name() -> String {
+		"foo"
+	}
+}
+
+// This also works for non-lifetime variables:
+#[derive(Name)]
+struct Foo;
+
+// Expands to this code:
+impl Foo {
+	pub fn name() -> String {
+		"foo"
+	}
+}
+ */
